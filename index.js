18b29221ce85ab66f1d46888efc478543dffbbef
kata-agata <agata.k.jazdzewska@gmail.com>
2020-09-28 22:18:09 +0200

    BFT implemented to search files including test.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const runner = new Runner();
 // helper function to test if works.
 //await must be in async function
 const run = async()=>{
-  const results = await runner.collectFiles(process.cwd());//process.cwd returns current directory
-  console.log(results);
+  await runner.collectFiles(process.cwd());//process.cwd returns current directory
+  console.log(runner.testFiles);
 }
 run();
--- runner.js
+++ runner.js
@@ -1,10 +1,24 @@
 const fs = require('fs');
+const path = require('path');
 class Runner{
   constructor(){
-    this.files = [];
+    this.testFiles = [];
   }
   async collectFiles(targetPath){
     const files = await fs.promises.readdir(targetPath);
+
+    for(let file of files){
+      const filepath = path.join(targetPath, file);
+      const stats = await fs.promises.lstat(filepath);
+      if (stats.isFile() && file.includes('.test.js')){
+        this.testFiles.push({name: filepath});
+      } else if(stats.isDirectory()){
+        const childFiles = await fs.promises.readdir(filepath);
+        files.push(...childFiles.map(f=> path.join(file,f)));//destructuring not to add an array but each item
+        // map function is adding direcotry name to the path
+      }
+    }
+
     return files;
   }
 }
